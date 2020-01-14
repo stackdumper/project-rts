f1c4efb137fd65ad07f5be97b8c7b6979ed07f48
Ilya Atamas <stackdumper@gmail.com>
2020-01-14 12:28:17 +0200

    fix(ui): prevent unit de-selection when clicking outside of canvas

--- src/systems/selection.ts
+++ src/systems/selection.ts
@@ -6,10 +6,12 @@ import { ResourceSelection } from '~/resources'
  * SystemSelection is used to add selected entities to selected resource.
  */
 export class SystemSelection extends System {
+  private canvas = document.getElementById('root')! as HTMLCanvasElement
+
   public initialize(core: Core) {
     const selection = core.getResource(ResourceSelection) as ResourceSelection
 
-    window.addEventListener('click', (e) => {
+    this.canvas.addEventListener('click', (e) => {
       selection.selected = []
 
       for (const entity of core.entities.values()) {
